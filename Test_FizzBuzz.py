265635d2233ed292bfe79b985e9e62ecbee7a960
AOUADI Rami <12007373@crit-xubuntu>
2023-10-02 11:39:53 +0200

    j

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -6,4 +6,5 @@ class Test_FizzBuzz(unittest.TestCase):
     def setUp(self) :
         self.instance=FizzBuzz()
 
-    
+    def test_affiche_sans_param(self):
+        self.assertEqual(self.instance.affiche(), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee")
