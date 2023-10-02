7bd06193c0652c9f44ae1c2a9cd8c16a7ad7ca3b
AOUADI Rami <12007373@crit-xubuntu>
2023-10-02 11:47:56 +0200

    affichage sans parametre

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -8,3 +8,4 @@ class Test_FizzBuzz(unittest.TestCase):
 
     def test_affiche_sans_param(self):
         self.assertEqual(self.instance.affiche(), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee")
+        
