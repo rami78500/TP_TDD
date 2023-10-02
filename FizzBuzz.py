c7a9ef24e97f8707c2d9582d270c2158f8ee5f81
AOUADI Rami <12007373@crit-xubuntu>
2023-10-02 11:52:38 +0200

    tqt

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,17 @@
 class FizzBuzz:
     def affiche():
         pass
+
+#!/usr/bin/python3
+
+def affiche():
+	for i in range(1,35):
+		if i%3==0 and i%5==0:
+			print("FrisBee")
+		elif i%5==0:
+			print("Buzz")
+		elif i%3==0:
+			print("Fizz")
+		else:
+			print(i)
+affiche()
